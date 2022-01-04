5b635298fa1319d87f1af935e4bc88437428b76d
LinuzJ <linusjern00@gmail.com>
2022-01-04 12:43:41 +0200

    bot tested, works

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,20 +3,20 @@ import bots.CoreBot
 
 object Main extends App {
 
-  private val key: Option[String] = sys.env.get("HOME")
+  private val key: Option[String] = sys.env.get("TELEGRAM_TOKEN")
   private var bot: CoreBot = _
 
-  println(key)
-//   if (key.isDefined) {
-//     bot = new TestBot(key.get)
-//   } else {
-//     Console.err.println("Please provide token in .env variable")
-//     System.exit(0)
-//   }
+  if (key.isDefined) {
+    bot = new TestBot(key.get)
+  } else {
+    Console.err.println("Please provide token in .env variable")
+    System.exit(0)
+  }
 
   val eol = bot.run()
   println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
   scala.io.StdIn.readLine()
-  bot.shutdown() // initiate shutdown
+
+  bot.shutdown()
 
 }

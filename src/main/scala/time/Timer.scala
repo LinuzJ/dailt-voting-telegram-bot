6ae86131b981b4b313f6e6e995a87af1937e005d
LinuzJ <linusjern00@gmail.com>
2022-01-05 22:54:52 +0200

    option adding to auto generated daily polls

--- src/main/scala/time/Timer.scala
+++ src/main/scala/time/Timer.scala
@@ -2,16 +2,16 @@ package time
 
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit
-import bots.CoreBot
+import bots.TestBot
 
 class Timer extends Runnable {
 
-  private var bot: CoreBot = _
+  private var bot: Option[TestBot] = None
   private var startTime: Calendar = Calendar.getInstance()
   private var currTime: Calendar = Calendar.getInstance()
 
   // Init bot
-  def setBot(newBot: CoreBot): Unit = bot = newBot
+  def setBot(newBot: TestBot): Unit = bot = Some(newBot)
 
   def getCurrTime(): Calendar = currTime
 
@@ -20,12 +20,25 @@ class Timer extends Runnable {
   )
 
   // Helpers
-  def getCurrentDate(): Int = currTime.get(Calendar.DATE)
+  def getCurrentDate(): String =
+    currTime.get(Calendar.DATE) + "-" + currTime.get(Calendar.MONTH)
   def getCurrentMinute(): Int = currTime.get(Calendar.MINUTE)
 
   def run() {
+
+    // Init first poll
+    bot match {
+      case a: Option[TestBot] => bot.get.newPoll(this.getCurrentDate())
+      case _                  =>
+    }
+
+    // Timer loop itself
     while (true) {
       currTime = Calendar.getInstance()
+
+      if (elapsedTime() == 60) {
+        println("mkpoll")
+      }
     }
   }
 }

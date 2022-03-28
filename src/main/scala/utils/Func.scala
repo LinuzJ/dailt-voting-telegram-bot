8ec76b1ace28a761b70246e11fe31eed70b648ab
LinuzJ <linusjern00@gmail.com>
2022-03-28 13:35:33 +0300

    Send out poll  results

--- src/main/scala/utils/Func.scala
+++ src/main/scala/utils/Func.scala
@@ -38,47 +38,59 @@ object Func {
       1. Sending out a poll to each chat with all options added today
       2. Closes the polls after specified period
       3. Sends a summary of the polls to each chat
+      4. Creates new polls for each chat
    */
   def timerTask(b: VotingBot, time: Int, counter: Counter): Unit = {
     val success: Boolean = b.makePolls()
-    if (success) {
-      b.chats.foreach(x =>
-        b.sendMessage(
-          s"The poll is open!\n You have ${time}s time to answer!",
-          x._1
-        )
-      )
-      Thread.sleep((time / 2) * 1000)
-      b.chats.foreach(x =>
-        b.sendMessage(
-          s"Half of the answering time is gone!\n You have ${time / 2}s left to answer!",
-          x._1
-        )
-      )
-      Thread.sleep((time / 4) * 1000)
-      b.chats.foreach(x =>
-        b.sendMessage(
-          s"Only 1/4 of the answering time left!\n You have ${time / 4}s left to answer!",
-          x._1
-        )
-      )
 
-      Thread.sleep((time / 4) * 1000)
-      b.chats.foreach(x =>
-        b.sendMessage(
-          s"Time is up!",
-          x._1
-        )
-      )
-      // Stop the current poll
-      b.stopPolls()
-
-      // Init new poll for each chat
-      b.chats.keySet.foreach(id => {
-        b.newPoll(id, counter.getCounter(), this.getCurrentDate())
-        counter.increment()
-      })
+    // Make sure everything went right before seniding polls
+    if (!success) {
+      println("Polls not successful")
     }
+
+    b.chats.foreach(x =>
+      b.sendMessage(
+        s"The poll is open!\n You have ${time}s time to answer!",
+        x._1
+      )
+    )
+    Thread.sleep((time / 2) * 1000)
+    b.chats.foreach(x =>
+      b.sendMessage(
+        s"Half of the answering time is gone!\n You have ${time / 2}s left to answer!",
+        x._1
+      )
+    )
+    Thread.sleep((time / 4) * 1000)
+    b.chats.foreach(x =>
+      b.sendMessage(
+        s"Only 1/4 of the answering time left!\n You have ${time / 4}s left to answer!",
+        x._1
+      )
+    )
+
+    Thread.sleep((time / 4) * 1000)
+    b.chats.foreach(x =>
+      b.sendMessage(
+        s"Time is up!",
+        x._1
+      )
+    )
+    // Stop the current poll
+    b.stopPolls()
+
+    // Send out results
+    b.chats.foreach(x =>
+      b.sendMessage(
+        x._2.toArray.sortBy(_._1).head._2.representation(),
+        x._1
+      )
+    )
+    // Init new poll for each chat
+    b.chats.keySet.foreach(id => {
+      b.newPoll(id, counter.getCounter(), this.getCurrentDate())
+      counter.increment()
+    })
   }
 
 }
